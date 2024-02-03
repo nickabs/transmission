89f3077af5a7a597be323c302ae2babde903c0b3
Nick Abson <4947488+nick-abson@users.noreply.github.com>
2024-02-03 18:41:18 +0000

    simplified gulpfile (esm)

--- assets/built/external.js
+++ assets/built/external.js
@@ -997,12 +997,28 @@ function jump (target, options) {
 
 /* eslint no-var: off */
 
+const SCROLL_LEEWAY = 30
 module.exports = function updateTocScroll (options) {
   var toc = options.tocElement || document.querySelector(options.tocSelector)
   if (toc && toc.scrollHeight > toc.clientHeight) {
     var activeItem = toc.querySelector('.' + options.activeListItemClass)
     if (activeItem) {
-      toc.scrollTop = activeItem.offsetTop - options.tocScrollOffset
+      // Determine container top and bottom
+      var cTop = toc.scrollTop
+      var cBottom = cTop + toc.clientHeight
+
+      // Determine element top and bottom
+      var eTop = activeItem.offsetTop
+      var eBottom = eTop + activeItem.clientHeight
+
+      // Check if out of view
+      // Above scroll view
+      if (eTop < cTop + options.tocScrollOffset) {
+        toc.scrollTop -= (cTop - eTop) + options.tocScrollOffset
+      // Below scroll view
+      } else if (eBottom > cBottom - options.tocScrollOffset - SCROLL_LEEWAY) {
+        toc.scrollTop += (eBottom - cBottom) + options.tocScrollOffset + (2 * SCROLL_LEEWAY)
+      }
     }
   }
 }
@@ -1058,7 +1074,7 @@ module.exports = function updateTocScroll (options) {
 /******/ 	
 /******/ })()
 ;
-
+//# sourceMappingURL=main.js.map
 
 /* **********************************************
      Begin prism-core.js
@@ -3061,109 +3077,6 @@ Prism.languages.js = Prism.languages.javascript;
 
 }(Prism));
 
-// https://www.json.org/json-en.html
-Prism.languages.json = {
-	'property': {
-		pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?=\s*:)/,
-		lookbehind: true,
-		greedy: true
-	},
-	'string': {
-		pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
-		lookbehind: true,
-		greedy: true
-	},
-	'comment': {
-		pattern: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,
-		greedy: true
-	},
-	'number': /-?\b\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
-	'punctuation': /[{}[\],]/,
-	'operator': /:/,
-	'boolean': /\b(?:false|true)\b/,
-	'null': {
-		pattern: /\bnull\b/,
-		alias: 'keyword'
-	}
-};
-
-Prism.languages.webmanifest = Prism.languages.json;
-
-// https://www.freedesktop.org/software/systemd/man/systemd.syntax.html
-
-(function (Prism) {
-
-	var comment = {
-		pattern: /^[;#].*/m,
-		greedy: true
-	};
-
-	var quotesSource = /"(?:[^\r\n"\\]|\\(?:[^\r]|\r\n?))*"(?!\S)/.source;
-
-	Prism.languages.systemd = {
-		'comment': comment,
-
-		'section': {
-			pattern: /^\[[^\n\r\[\]]*\](?=[ \t]*$)/m,
-			greedy: true,
-			inside: {
-				'punctuation': /^\[|\]$/,
-				'section-name': {
-					pattern: /[\s\S]+/,
-					alias: 'selector'
-				},
-			}
-		},
-
-		'key': {
-			pattern: /^[^\s=]+(?=[ \t]*=)/m,
-			greedy: true,
-			alias: 'attr-name'
-		},
-		'value': {
-			// This pattern is quite complex because of two properties:
-			//  1) Quotes (strings) must be preceded by a space. Since we can't use lookbehinds, we have to "resolve"
-			//     the lookbehind. You will see this in the main loop where spaces are handled separately.
-			//  2) Line continuations.
-			//     After line continuations, empty lines and comments are ignored so we have to consume them.
-			pattern: RegExp(
-				/(=[ \t]*(?!\s))/.source +
-				// the value either starts with quotes or not
-				'(?:' + quotesSource + '|(?=[^"\r\n]))' +
-				// main loop
-				'(?:' + (
-					/[^\s\\]/.source +
-					// handle spaces separately because of quotes
-					'|' + '[ \t]+(?:(?![ \t"])|' + quotesSource + ')' +
-					// line continuation
-					'|' + /\\[\r\n]+(?:[#;].*[\r\n]+)*(?![#;])/.source
-				) +
-				')*'
-			),
-			lookbehind: true,
-			greedy: true,
-			alias: 'attr-value',
-			inside: {
-				'comment': comment,
-				'quoted': {
-					pattern: RegExp(/(^|\s)/.source + quotesSource),
-					lookbehind: true,
-					greedy: true,
-				},
-				'punctuation': /\\$/m,
-
-				'boolean': {
-					pattern: /^(?:false|no|off|on|true|yes)$/,
-					greedy: true
-				}
-			}
-		},
-
-		'punctuation': /=/
-	};
-
-}(Prism));
-
 (function (Prism) {
 	// $ set | grep '^[A-Z][^[:space:]]*=' | cut -d= -f1 | tr '\n' '|'
 	// + LC_ALL, RANDOM, REPLY, SECONDS.
@@ -3639,4 +3552,3 @@ Prism.languages.webmanifest = Prism.languages.json;
 	});
 
 }());
-//# sourceMappingURL=vendor.min.js.map
