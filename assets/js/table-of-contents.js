db106a5b594c0d68dc1282295f2cd7ebac8d9bf5
Nick Abson <4947488+nick-abson@users.noreply.github.com>
2024-05-06 08:18:34 +0100

    accent color improvements

--- assets/js/table-of-contents.js
+++ assets/js/table-of-contents.js
@@ -66,8 +66,8 @@
     // this is used to update the circular progress bar show around the toc button
     tocToggleButton.style.setProperty(
     '--conic-gradient',
-    `var(--accent-color) 0deg 0%,  
-      var(--accent-color) 0deg ${ progress.toFixed(3) }%,
+    `var(--theme-accent-color) 0deg 0%,  
+      var(--theme-accent-color) 0deg ${ progress.toFixed(3) }%,
       var(--background-color) 0deg ${ 1 - progress.toFixed(3) }%,
       var(--background-color) 0deg 360deg`,
     );
