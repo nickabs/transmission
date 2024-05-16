8c890e9560c416ba2067087ca9e00cf1bfad7921
Nick Abson <4947488+nick-abson@users.noreply.github.com>
2024-05-16 07:56:38 +0100

    improved drop down menu + new expanded/minified menu custom option

--- assets/js/dropdown-menu.js
+++ assets/js/dropdown-menu.js
@@ -18,100 +18,108 @@
   let i, match, submenu;
   let newElement, menuName
 
-  // use a superscript unicode down arrow as a drop down indicator 
-  let dropdownSymbol= document.createElement("sup"); 
-  dropdownSymbol.textContent=""
-
-  /*
-  go throught the menu items and create the html structure shown above
-  the code expects the following html for each item:
-  */
+  /* go throught the menu items and create the html structure shown above */
   for (i = 0; i < menuItems.length; ++i) {
 
     match = menuItems[i].lastElementChild.innerText.match(menuIndent);
 
     if (match) {
-      // remove the submenu & subitem indent indicators (- --) 
+      /* remove the submenu & subitem indent indicators (- --) */
       menuItems[i].lastElementChild.innerText = menuItems[i].lastElementChild.innerText.replace(menuIndent,"$2");
       menuName=menuItems[i].lastElementChild.innerText;
 
       if (match[1].length == 1) {
+        /* create parent elemement for the submenu */
         menuItems[i].classList.add("has-submenu");
-        menuItems[i].replaceChildren(document.createElement("a"));
-        menuItems[i].querySelector('a').setAttribute("href","#");
-        menuItems[i].querySelector('a').appendChild(document.createTextNode(menuName));
-        menuItems[i].querySelector('a').appendChild(document.createElement('sup'));
-        menuItems[i].querySelector('sup').appendChild(document.createTextNode('⌄'));
-        menuItems[i].appendChild(document.createElement('ul'));
-        menuItems[i].querySelector('ul').classList="submenu";
-        menuItems[i].querySelector('a').classList.add('tabindex=0');
+
+        const anchor = document.createElement("a");
+        anchor.setAttribute("href", "#");
+        anchor.classList.add('tabindex=0');
+        anchor.appendChild(document.createTextNode(menuName));
+
+        const sup = document.createElement('sup');
+        sup.appendChild(document.createTextNode(' ⌄'));
+        anchor.appendChild(sup);
+
+        const ul = document.createElement('ul');
+        ul.classList.add("submenu");
+
+        const span = document.createElement('span');
+        span.classList.add('submenu-header');
+
+        const p1 = document.createElement('p');
+        p1.classList.add('submenu-title');
+        p1.appendChild(document.createTextNode(menuName));
+        span.appendChild(p1);
+
+        const p2 = document.createElement('p');
+        p2.classList.add('submenu-close');
+        p2.appendChild(document.createTextNode('✕'));
+        span.appendChild(p2);
+
+        ul.appendChild(span);
+
+        menuItems[i].replaceChildren(anchor);
+        menuItems[i].appendChild(ul);
         submenu=menuItems[i];
       }
       else if (submenu) {
+        /* add submenu items */
         menuItems[i].classList.add("subitem");
         menuItems[i].classList.remove("item");
         submenu.getElementsByTagName("ul")[0].appendChild(menuItems[i]);
       }
     }
   }
-  // set this class so that the menu items remain hidden until this script finishes
+  /* set this class so that the menu items remain hidden until this script finishes */
   document.querySelector(".menu").classList.add("menu-ready");
 })();
 
 /* 
  * toggle menu and submenus
- * .site.menu-open is set when the menu is opened using the hamburger button (used on small screens)
- * item.submenu-open is set when an item with a submenu is clicked (can be used on both mobile and widescreen)
+ * .menu.menu-open is set when the menu is opened using the hamburger button 
+ * .item.submenu-open is set when an item with a submenu is opened
 */
 (function() {
-  const openMenuButton = document.querySelector(".open-menu");
-  const closeMenuButton = document.querySelector(".close-menu");
-  const site = document.querySelector(".site");
+  const toggleMenuButton = document.querySelector(".menu-toggle");
+  const closeMenuButton = document.querySelector(".menu-close");
+  const menu = document.querySelector(".menu");
   const submenuItems = document.querySelectorAll(".has-submenu");
 
   function toggleMenu(){
-      site.classList.toggle("menu-open");
+      menu.classList.toggle("menu-open");
   }
 
   /* toggle submenu */
-  function toggleItem() {
+  function toggleSubmenu() {
     if (this.classList.contains("submenu-open")) { // this submenu is already active
       this.classList.remove("submenu-open");
-    } else if (site.querySelector(".submenu-open")) {  // another submenu is active
-      site.querySelector(".submenu-open").classList.remove("submenu-open");
+    } else if (menu.querySelector(".submenu-open")) {  // another submenu is active
+      menu.querySelector(".submenu-open").classList.remove("submenu-open");
       this.classList.add("submenu-open");
     } else {
       this.classList.add("submenu-open"); 
     }
-
   }
 
-  /* close menus on click */
+  /* close submenu on click */
   window.addEventListener('click', function(e){
-    // close submenu when clicking anywhere on the menu 
-    if (site.querySelector(".submenu-open")) {
-      let isClickInside = site.querySelector(".submenu-open").contains(e.target);
-
-      if (!isClickInside && site.querySelector(".submenu-open")) {
-        site.querySelector(".submenu-open").classList.remove("submenu-open");
+    if (menu.querySelector(".submenu-open")) {
+      if (!menu.querySelector(".submenu-open").contains(e.target)) {
+        menu.querySelector(".submenu-open").classList.remove("submenu-open");
       }
-    }
-    // close menu when clicking ouside the menu 
-    if (!site.contains(e.target) && !openMenuButton.contains(e.target) &&
-       site.classList.contains("menu-open")) {
-          toggleMenu();
     }
   });
 
   /* Toggle mobile menu */
-  openMenuButton.addEventListener('click', toggleMenu, false);
+  toggleMenuButton.addEventListener('click', toggleMenu, false);
   closeMenuButton.addEventListener('click', toggleMenu, false);
 
   /* toggle submenu items */
   for (let item of submenuItems) {
     if (item.querySelector(".submenu")) {
-      item.addEventListener("click", toggleItem, false);
+      item.addEventListener("click", toggleSubmenu, false);
     }
-    item.addEventListener("keypress", toggleItem, false);
+    item.addEventListener("keypress", toggleSubmenu, false);
   }
 })();
