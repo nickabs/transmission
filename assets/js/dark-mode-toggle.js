e0cd2e604a759d070b7e3c82c1fb5d18111ee046
Nick Abson <4947488+nick-abson@users.noreply.github.com>
2024-06-06 07:53:54 +0100

    support for comments / removed reduntant option for subs banner on posts

--- assets/js/dark-mode-toggle.js
+++ assets/js/dark-mode-toggle.js
@@ -1,27 +1,36 @@
+/*
+ * toggle between dark and light mode
+ * if the user has set a system preference then default to that setting
+ * if they specify a setting save it in local storage so it is remembered next time
+ * 
+*/
 
 (function() {
     var darkModeToggle = document.getElementById("dark-mode-toggle");
 
-    if (darkModeToggle) {
-        var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-        if (storedTheme)
-            document.documentElement.setAttribute('data-theme', storedTheme)
+    if (! darkModeToggle) {
+        return;
+    }
 
-        setTimeout(function() {
-            var targetTheme = storedTheme == "dark" ? "light" : "dark";
-        }, 1000);
+    var storedTheme = localStorage.getItem('data-color-scheme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 
-        darkModeToggle.onclick = function() {
-            var currentTheme = document.documentElement.getAttribute("data-theme");
-            var targetTheme = "light";
+    if (storedTheme) {
+        document.documentElement.setAttribute('data-color-scheme', storedTheme);
+    }
 
-            if (currentTheme === "light") {
-                targetTheme = "dark";
-            }
+    darkModeToggle.onclick = function() {
+        var currentTheme = document.documentElement.getAttribute("data-color-scheme");
+        var targetTheme = currentTheme == "dark" ? "light" : "dark";
 
-            document.documentElement.setAttribute('data-theme', targetTheme)
-            localStorage.setItem('theme', targetTheme);
+        document.documentElement.setAttribute('data-color-scheme', targetTheme)
+        localStorage.setItem('data-color-scheme', targetTheme);
 
+        // set color-scheme on the comments iframe
+        // As of 5.0 Ghost helper supports {{comments mode="auto" }} which will use the background color to determine if the theme is using a dark mode
+        // ... but this only works after a screen refresh.  The code below will make the scheme change on clicking the dark mode toggle
+        const commentsScript = document.querySelector('script[data-color-scheme]');
+        if (commentsScript) {
+            commentsScript.setAttribute('data-color-scheme', targetTheme);
         };
     }
-})();
+  })();
